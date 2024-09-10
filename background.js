6b693e24ca601e6a1e634be11092b3da77fee0eb
Pham Van Hung <phamvanhung.dev@gmail.com>
2024-09-10 22:32:57 +0700

    toggle

--- background.js
+++ background.js
@@ -1,31 +1,33 @@
+chrome.downloads.onChanged.addListener((downloadDelta) => {
+    if (downloadDelta.state && downloadDelta.state.current === "complete") {
+        chrome.downloads.search({ id: downloadDelta.id }, (results) => {
+            if (results.length && results[0].state === "complete") {
+                chrome.downloads.search({ id: downloadDelta.id }, function (downloads) {
+                    if (downloads && downloads.length > 0) {
+                        let downloadedFile = downloads[0];
+                        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                            if (tabs.length > 0) {
+                                chrome.tabs.sendMessage(tabs[0].id, {
+                                    action: "fileDownloaded",
+                                    filePath: downloadedFile.filename
+                                });
+                            }
+                        });
+                    }
+                });
+                return true;
+            }
+        });
+    }
+});
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     switch (request.action) {
-        case "getCookies": {
-            chrome.cookies.getAll({ domain: request.domain }, function (cookies) {
-                sendResponse({ cookies: cookies });
-            });
-            return true;
-        }
-        case "buttonClicked": {
-            chrome.downloads.onChanged.addListener(function (downloadDelta) {
-                if (downloadDelta.state && downloadDelta.state.current === "complete") {
-                    chrome.downloads.search({ id: downloadDelta.id }, function (downloads) {
-                        if (downloads && downloads.length > 0) {
-                            let downloadedFile = downloads[0];
-                            chrome.tabs.sendMessage(sender.tab.id, {
-                                action: "fileDownloaded",
-                                filePath: downloadedFile.filename
-                            });
-                        }
-                    });
-                }
-            });
-            return true;
-        }
         case "readFile": {
             fetch('file://' + request.filePath)
                 .then(response => response.arrayBuffer())
                 .then(buffer => {
+                    console.log(buffer);
                     sendResponse({ success: true, fileContent: buffer });
                 })
                 .catch(error => {
@@ -35,3 +37,4 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         }
     }
 });
+
