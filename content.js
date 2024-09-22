35b765c8723102b19e2aa48412f44e254d1819a4
Pham Van Hung <phamvanhung.dev@gmail.com>
2024-09-22 12:40:29 +0700

    done

--- content.js
+++ content.js
@@ -8,6 +8,25 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     return true;
 });
 
+var btnCloseClass = '.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x16tdsg8.xggy1nq.x1ja2u2z.x1t137rt.x6s0dn4.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x3nfvp2.xdl72j9.x1q0g3np.x2lah0s.x193iq5w.x1n2onr6.x1hl2dhg.x87ps6o.xxymvpz.xlh3980.xvmahel.x1lku1pv.x1g40iwv.x1g2r6go.x16e9yqp.x12w9bfk.x15406qy.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.xo1l8bm.xbsr9hj.x1v911su.x1y1aw1k.xwib8y2.x1ye3gou.xn6708d';
+    var buttonDownloadTrueClass = '.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x16tdsg8.xggy1nq.x1ja2u2z.x1t137rt.x6s0dn4.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x3nfvp2.xdl72j9.x1q0g3np.x2lah0s.x193iq5w.x1n2onr6.x1hl2dhg.x87ps6o.xxymvpz.xlh3980.xvmahel.x1lku1pv.x1g40iwv.x1g2r6go.x16e9yqp.x12w9bfk.x15406qy.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.xo1l8bm.x140t73q.x19bke7z.x1y1aw1k.xwib8y2.x1swvt13.x1pi30zi';
+var classModel = '.x9f619.xxc7z9f.x13faqbe.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.x1ine8nr.xmi5d70.x1fvot60.xo1l8bm.xxio538.x1gzqxud.xbsr9hj.x2lah0s.xyamay9.x1l90r2v.x1swvt13.x1pi30zi.x1mbqufl.x6o7n8i.xw7d9y7.x12w9bfk.x1hc1fzr.x1q2yuad';
+var btnDownloadClass = '.x1xqt7ti.x1fvot60.xk50ysn.xxio538.x1heor9g.xuxw1ft.x6ikm8r.x10wlt62.xlyipyv.x1h4wwuj.xeuugli';
+var i = 0;
+setInterval(async function () {
+    let btnDownloadTrue = document.querySelector(classModel + ' ' + buttonDownloadTrueClass);
+    let btnClose = document.querySelector(classModel + ' ' + btnCloseClass);
+    await delay(500);
+    if (btnDownloadTrue && !isDownloading) {
+        console.log('send action');
+        btnDownloadTrue.click();
+        isDownloading = true;
+        setTimeDownload();
+        await delay(5000);
+        btnClose.click();
+    }
+}, 1000);
+
 async function openModal() {
     try {
         let btnOpenPopup = document.querySelectorAll("#mediaManagerExportInsightsButton .x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x16tdsg8.xggy1nq.x1ja2u2z.x6s0dn4.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x3nfvp2.xdl72j9.x1q0g3np.x2lah0s.x193iq5w.x1n2onr6.x1hl2dhg.x87ps6o.xxymvpz.xlh3980.xvmahel.x1lku1pv.x1g40iwv.x1g2r6go.x16e9yqp.x12w9bfk.x15406qy.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r");
@@ -26,7 +45,7 @@ async function openModal() {
 }
 
 
-let isDownloading = false;
+var isDownloading = false;
 async function downloadFile() {
 
     try {
@@ -39,57 +58,10 @@ async function downloadFile() {
         };
 
         let btnClass = '.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x16tdsg8.xggy1nq.x1ja2u2z.x1t137rt.x6s0dn4.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x3nfvp2.xdl72j9.x1q0g3np.x2lah0s.x193iq5w.x1n2onr6.x1hl2dhg.x87ps6o.xxymvpz.xlh3980.xvmahel.x1lku1pv.x1g40iwv.x1g2r6go.x16e9yqp.x12w9bfk.x15406qy.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.xo1l8bm.x140t73q.x19bke7z.x1y1aw1k.xwib8y2.x1swvt13.x1pi30zi';
-        let btnCloseClass = '.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x16tdsg8.xggy1nq.x1ja2u2z.x1t137rt.x6s0dn4.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x3nfvp2.xdl72j9.x1q0g3np.x2lah0s.x193iq5w.x1n2onr6.x1hl2dhg.x87ps6o.xxymvpz.xlh3980.xvmahel.x1lku1pv.x1g40iwv.x1g2r6go.x16e9yqp.x12w9bfk.x15406qy.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.xo1l8bm.xbsr9hj.x1v911su.x1y1aw1k.xwib8y2.x1ye3gou.xn6708d';
-        let buttonDownloadTrueClass = '.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x16tdsg8.xggy1nq.x1ja2u2z.x1t137rt.x6s0dn4.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x3nfvp2.xdl72j9.x1q0g3np.x2lah0s.x193iq5w.x1n2onr6.x1hl2dhg.x87ps6o.xxymvpz.xlh3980.xvmahel.x1lku1pv.x1g40iwv.x1g2r6go.x16e9yqp.x12w9bfk.x15406qy.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.xo1l8bm.x140t73q.x19bke7z.x1y1aw1k.xwib8y2.x1swvt13.x1pi30zi';
+        
 
         let btnCreateFile = getInp(btnClass);
         btnCreateFile.click();
-        await delay(5000);
-        let classModel = '.x9f619.xxc7z9f.x13faqbe.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.x1ine8nr.xmi5d70.x1fvot60.xo1l8bm.xxio538.x1gzqxud.xbsr9hj.x2lah0s.xyamay9.x1l90r2v.x1swvt13.x1pi30zi.x1mbqufl.x6o7n8i.xw7d9y7.x12w9bfk.x1hc1fzr.x1q2yuad';
-        let btnDownloadClass = '.x1xqt7ti.x1fvot60.xk50ysn.xxio538.x1heor9g.xuxw1ft.x6ikm8r.x10wlt62.xlyipyv.x1h4wwuj.xeuugli';
-        let btnDownload = document.querySelector(classModel + ' ' + btnDownloadClass);
-        let btnClose = document.querySelector(classModel + ' ' + btnCloseClass);
-        await delay(500);
-        let downloadInterval;
-        if (btnDownload && !isDownloading) {
-            console.log('true download');
-            isDownloading = true;
-
-            // Khởi tạo MutationObserver để theo dõi thay đổi thuộc tính
-            let observer = new MutationObserver((mutationsList, observer) => {
-                for (let mutation of mutationsList) {
-                    if (mutation.type === 'attributes') {
-                        console.log('ok');
-                    }
-                }
-            });
-            // Thiết lập cấu hình để theo dõi thay đổi thuộc tính
-            let config = { attributes: true };
-            // Bắt đầu quan sát btnDownload
-            observer.observe(btnDownload, config);
-
-            downloadInterval = setInterval(async () => {
-                let btnDownloadTrue = document.querySelector(classModel + ' ' + buttonDownloadTrueClass);
-                if (btnDownloadTrue && isDownloading) {
-                    console.log('send action');
-                    btnDownloadTrue.click();
-                    isDownloading = false;
-                    setTimeDownload();
-                    await delay(5000);
-                    btnClose.click();
-                    clearInterval(downloadInterval);
-                    return;
-                }
-                if (window.location.href.startsWith('https://business.facebook.com/')) {
-                    if (!document.querySelector(classModel + ' ' + btnDownloadClass)) {
-                        isDownloading = false;
-                        setTimeDownload();
-                        clearInterval(downloadInterval);
-                        return;
-                    }
-                }
-            }, 1000);
-        }
     } catch (e) {
         location.reload();
         setTimeDownload();
@@ -101,7 +73,7 @@ async function filterData() {
     await delay(200);
 
     // Chọn trang
-    if(!listInp[0]) return false;
+    if (!listInp[0]) return false;
     listInp[0].click();
     await delay(1000);
     let listPage = document.querySelectorAll('.x1iyjqo2.x1iorvi4.x150jy0e.xjkvuk6.x1e558r4.x1t137rt.x78zum5.xdt5ytf.xu3tz18.x1oux0zi .x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.x78zum5.xdl72j9.xdt5ytf.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt');
